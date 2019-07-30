5679e2c8017debe40a2c6e7ddd77199fe65f4a8c
graysonlee123 <graysonleegantek@gmail.com>
2019-07-30 18:11:01 -0400

    Added input box and return user input

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+const apiKey = "t9Js0eys1AyskcYxqnMTzaI3ksBDn9hy";
+
+$.ajax({
+    url: `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=`,
+    method: "GET"
+}).then(function (data) {
+    console.log(data);
+});
+
+$("#submit").on("click", function (e) {
+    //Prevents page from refreshing
+    e.preventDefault();
+    
+    console.log($("#input").val());
+});
